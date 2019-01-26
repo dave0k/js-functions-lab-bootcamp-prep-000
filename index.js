6f8f83a280e9e4bf3a6a3d36bcef618d1d68ab00
Dave K <dkorff12@gmail.com>
2019-01-26 04:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+
+function happyHolidays() {
+  console.log('Happy holidays!');
+}
+
+happyHolidays();
+
+function happyHolidaysTo(x) {
+  console.log(`Happy holidays, ${x}!`);
+}
+
+happyHolidsaysTo();
